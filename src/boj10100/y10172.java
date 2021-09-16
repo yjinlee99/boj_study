563183cf51f2316c747a1d0e70326531ec4543e6
yjinlee <yjinlee99@gmail.com>
2021-09-16 18:26:00 +0900

    개

---
+++ src/boj10100/y10172.java
@@ -0,0 +1,11 @@
+package boj10100;
+
+public class y10172 {
+    public static void main(String[] args) {
+        System.out.printf("|\\_/|\n");
+        System.out.printf("|q p|   /}\n");
+        System.out.printf("( 0 )\"\"\"\\\n");
+        System.out.printf("|\"^\"`    |\n");
+        System.out.printf("||_/=\\\\__|\n");
+    }
+}
