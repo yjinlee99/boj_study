27048ae365c926ffcc8664fd616b7738711dc6fe
yjinlee <yjinlee99@gmail.com>
2021-09-16 19:11:58 +0900

    a/b

---
+++ src/boj1000/y1008.java
@@ -0,0 +1,13 @@
+package boj1000;
+
+import java.util.Scanner;
+
+public class y1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        System.out.println(a/b);
+    }
+}
