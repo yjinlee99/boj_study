c318746218f4930ca87e4ce8f7b440f83ee7f066
yjinlee <yjinlee99@gmail.com>
2021-09-16 18:13:26 +0900

    고양이

---
+++ src/boj10100/y10171.java
@@ -0,0 +1,10 @@
+package boj10100;
+
+public class y10171 {
+    public static void main(String[] args) {
+        System.out.printf("\\    /\\ \n");
+        System.out.printf(" )  ( ')\n");
+        System.out.printf("(  /  )\n");
+        System.out.printf(" \\(__)|\n");
+    }
+}
