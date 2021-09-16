a31030b22c0b724d9441b12d2c4299c7f87e6d4e
yjinlee <yjinlee99@gmail.com>
2021-09-16 18:07:28 +0900

    We love kriii

---
+++ src/boj10700/y10718.java
@@ -0,0 +1,8 @@
+package boj10700;
+
+public class y10718 {
+    public static void main(String[] args) {
+        System.out.printf("강한친구 대한육군\n");
+        System.out.printf("강한친구 대한육군\n");
+    }
+}
