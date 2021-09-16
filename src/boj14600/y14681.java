913d2c88a0e69bce127c3f7f33ec1c95f65b9994
yjinlee <yjinlee99@gmail.com>
2021-09-16 20:10:48 +0900

    사분면 고르기

---
+++ src/boj14600/y14681.java
@@ -0,0 +1,17 @@
+package boj14600;
+
+import java.util.Scanner;
+
+public class y14681 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a>0 && b>0) System.out.println(1);
+        else if (a<0 && b>0) System.out.println(2);
+        else if (a<0 && b<0) System.out.println(3);
+        else System.out.println(4);
+    }
+}
