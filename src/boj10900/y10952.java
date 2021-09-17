bbb04fd5e8739a8fc75c7b7e904ba8989475d893
yjinlee <yjinlee99@gmail.com>
2021-09-18 01:09:37 +0900

    a+b-5

---
+++ src/boj10900/y10952.java
@@ -0,0 +1,18 @@
+package boj10900;
+
+import java.util.Scanner;
+
+public class y10952 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            if((a == 0) && (b == 0)) break;
+
+            System.out.println(a + b);
+        }
+    }
+}
