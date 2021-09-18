299f2da8bf0d43036ee3fc7f7044e3ff6a7e1e38
yjinlee <yjinlee99@gmail.com>
2021-09-19 01:44:01 +0900

    한수

--- src/boj1000/y1065.java
+++ src/boj1000/y1065.java
@@ -11,9 +11,18 @@ public class y1065 {
         if(n < 100) res = n;
         else if (n < 111) res = 99;
         else{
-            for(int i=111;i<=n;i++){
-                i=0;
+            res = 99;
+            for(int i=111;i<=n;i++) {
+                int j = i;
+                int a = j % 10;
+                j = j / 10;
+                int b = j % 10;
+                int c = j / 10;
+
+                if ((a - b) == (b - c)) res++;
             }
         }
+
+        System.out.println(res);
     }
 }
