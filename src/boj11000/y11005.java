184ffc80762215cf162021cc4ddd28c0c4e679bb
yjin_lap <yjinlee99@naver.com>
2023-12-05 18:55:26 +0900

    2745,11005 완료

---
+++ src/boj11000/y11005.java
@@ -0,0 +1,13 @@
+package boj11000;
+
+import java.util.Scanner;
+
+public class y11005 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+
+    }
+}
--- src/boj2700/y2745.java
+++ src/boj2700/y2745.java
@@ -7,7 +7,21 @@ public class y2745 {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
         int n = sc.nextInt();
+        int answer = 0;
+        int j = 0;
 
-
+        if(n == 10) System.out.println(s);
+        else {
+            for(int i=s.length()-1; i >=0; i--) {
+                if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+                    answer += (int) (Math.pow(n, j) * (int) (s.charAt(i) - '0'));
+                }
+                else {
+                    answer +=  (int) (Math.pow(n, j) * (int) (s.charAt(i) - 64 + 9));
+                }
+                j++;
+            }
+            System.out.println(answer);
+        }
     }
 }
